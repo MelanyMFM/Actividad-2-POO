91c23b28af537bb43bb63642fe44e36d68c0fffb
srodriguezsu <srodriguezsu@unal.edu.co>
2023-08-26 18:01:18 -0500

    Ejercicio 11 Finalizado

--- Ejercico11.java
+++ Ejercico11.java
@@ -4,10 +4,24 @@ import java.util.Scanner;
 
 public class Ejercico11 {
     public static void main(String[] args) {
-        int N1, N2, N3;
+        int N1, N2, N3, mayor;
         Scanner input = new Scanner(System.in);
-        System.out.print("Inserte el primer numero:");
+        System.out.print("Inserte el primer numero: ");
         N1 = input.nextInt();
-        System.out.print("Inserte el primer numero:");
+        System.out.print("Inserte el segundo numero: ");
+        N2 = input.nextInt();
+        System.out.print("Inserte el tercer numero: ");
+        N3 = input.nextInt();
+        if (N1 > N2 && N1 > N3) {
+            mayor = N1;
+        } else{
+            if (N2>N3){
+                mayor = N2;
+            } else{
+                mayor = N3;
+            }
+        }
+        System.out.println("El mayor valor entre " + N1 +
+                ", " + N2 + " y " + N3 + " es: " + mayor);
     }
 }
