f913a59b8b1b2701a2964056d3956d21878a7b07
MelanyMFM <142820807+MelanyMFM@users.noreply.github.com>
2023-08-31 18:32:40 -0500

    Terminada clase rectangulo

--- parte2/Rectangulo.java
+++ parte2/Rectangulo.java
@@ -1,4 +1,17 @@
 package ejercicio.pkg18.parte2;
 
 public class Rectangulo {
+  int base, altura;
+  
+Rectangulo(int base, int altura) {
+  this.base = base;
+  this.altura = altura;
+  }
+  
+  double calcularArea() {
+    return base * altura
+  }
+  double calcularPerimetro() {
+    return (base * 2) + (altura * 2)
+  }
 }
